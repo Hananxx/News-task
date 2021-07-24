e63bfec7f4ea1c8f1b15b81d6459923148333e76
Hanan <hanan.hakami@tuwaiq.edu.sa>
2021-07-25 02:50:21 +0300

    add: main page styles

--- NewsPortal/resources/views/articles/show.blade.php
+++ NewsPortal/resources/views/articles/show.blade.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="/Users/hanans/Desktop/phpTask/News/NewsPortal/resources/css/app.css">
 <x-app-layout>
     <section class="pt-14 grid grid-cols-6 m-auto px-1 lg:px-3 gap-2 lg:gap-3">
         <div class="bg-white mt-12 rounded-lg">
@@ -20,7 +19,7 @@
         <div class="col-span-3 ">
            <h1 class="text-2xl font-bold">{{$article->category->name}}</h1>
             <div class="mt-4 bg-white p-2 rounded-lg">
-                <div style="background-image: url({{$article->cover_img}})" class="h-48  bg-no-repeat bg-center bg-cover rounded-xl"></div>
+                <div style="background-image: url({{$article->cover_img}})" class="h-64 bg-no-repeat bg-center bg-cover rounded-xl"></div>
                 <div class="px-3">
                     <div class="text-xs font-bold flex justify-between px-2 mt-3">
                         <span class="p-1 px-3 bg-indigo-100 rounded-full text-indigo-600 uppercase">{{$article->category->name}}</span>
@@ -38,12 +37,12 @@
                     </div>
                     <div class="flex justify-between">
                         <div class="">
-                            <h1 class="text-2xl font-bold my-2">{{$article->title}}</h1>
+                            <h1 class="text-2xl font-bold my-2 pr-2">{{$article->title}}</h1>
                             <h4 class="text-gray-600 text-sm">by <span class="font-bold uppercase">{{$article->author_name}}</span></h4>
                             <h4 class="text-xs text-gray-500">{{ \Illuminate\Support\Str::limit($article->created_at, 10, $end='') }}</h4>
 
                         </div>
-                        <div class=" mt-2">
+                        <div class="mt-4 w-full ">
                             <div class="px-2 p-1 text-xs font-bold border border-gray-500 rounded-full text-gray-500 flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
@@ -54,7 +53,7 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-4 px-2">
+            <div class="mt-4 px-2 leading-relaxed">
                 {!! $article->content !!}
             </div>
 
@@ -66,7 +65,7 @@
             <div class="">
 
                 @if($relatedArticles->count() == 1)
-                    <h1 class="text-sm mt-4 text-gray-600"><span class="font-medium">no avaliable new..</h1>
+                    <h1 class="text-sm mt-4 text-gray-600"><span class="font-medium">No available news...</h1>
 
                 @endif
                @foreach($relatedArticles as $related)
--- NewsPortal/resources/views/welcome.blade.php
+++ NewsPortal/resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 {{--</x-app-layout>--}}
 <x-app-layout>
     <section class="pt-14 grid grid-cols-6 m-auto px-1 lg:px-3 gap-2 lg:gap-3">
-        <div class="bg-white mt-12 rounded-lg">
+        <div class="hidden md:block bg-white mt-12 rounded-lg h-11/12">
             <h3 class="font-bold text-xl mb-2 p-3">Category</h3>
             <div class="flex flex-col leading-9 h-full px-3 text-gray-600">
                 @foreach($categories as $category)
@@ -43,15 +43,14 @@
                 @endforeach
             </div>
         </div>
-        <div class="col-span-3 ">
-
-            <h1 class="text-2xl font-bold">Latest Headlines</h1>
+        <div class="col-span-4 md:col-span-3">
+            <h1 class="text-2xl font-bold">Latest Headline</h1>
             <div class="mt-4 bg-white p-2 rounded-lg shadow-xl">
                 <div style="background-image: url({{$articles[0]->cover_img}})" class="h-64 bg-no-repeat bg-center bg-cover rounded-xl"></div>
                 <div class="px-3">
                     <div class="text-xs font-bold flex justify-between px-2 mt-3">
                         <span class="p-1 px-3 bg-indigo-100 rounded-full text-indigo-600 uppercase">{{$articles[0]->category->name}}</span>
-                        <div class="text-gray-700 flex items-center ">
+                        <div class="hidden md:flex text-gray-700  items-center ">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
@@ -69,8 +68,7 @@
 
                         </div>
                         <div class=" mt-2">
-                            <div class="px-2 p-1 text-xs font-bold bg-yellow-primary rounded-full text-yellow-500 flex items-center">
-
+                            <div class="px-2 md:px-4 p-1 text-xs font-bold hover:bg-blue-50 transition duration-300 ease-in-out border border-blue-400 rounded-full text-blue-500 flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                                 </svg>
@@ -80,15 +78,17 @@
                     </div>
                 </div>
             </div>
+            <h1 class="text-xl font-bold mt-11">More <span class="font-light">Headlines</span> </h1>
+
             <div class="grid md:grid-cols-2 gap-2 my-4">
                 @foreach($articles as $article)
                     @if($article->id != $articles[0]->id)
                         <div class="bg-white overflow-hidden rounded-lg shadow-xl">
-                            <div class="h-24 rounded-lg bg-center bg-no-repeat bg-cover" style="background-image: url({{$article->cover_img}})"></div>
-                            <div class="p-1 mt-1">
+                            <div class="h-32 rounded-lg bg-center bg-no-repeat bg-cover" style="background-image: url({{$article->cover_img}})"></div>
+                            <div class="p-1 px-2 mt-1">
                                 <span class="p-0.5 px-2 bg-yellow-100 rounded-full text-yellow-600 uppercase text-xs font-medium">{{$article->category->name}}</span>
-
-                                <h5 class="text-lg font-medium capitalize">{{$article->title}}</h5>
+                                <h5 class="text-lg font-medium capitalize mt-2">{{$article->title}}</h5>
+                                <h4 class="uppercase text-gray-600 text-xs my-1">{{$article->category->name}} | {{$article->user->name}}</h4>
 
                             </div>
                         </div>
